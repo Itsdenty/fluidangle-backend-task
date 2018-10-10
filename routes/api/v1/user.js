67ee79cbef9a8f9149f48bf4403a6f3ddf84bd67
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-10-10 12:31:43 +0000

    Add: user routes

---
+++ routes/api/v1/user.js
@@ -0,0 +1,11 @@
+import express from 'express';
+// import controller from '../../../controllers/user'
+
+// const router = express.Router();
+const router = express.Router();
+
+router.post('/');
+router.post('/login');
+router.get('/logout');
+
+export default router;
